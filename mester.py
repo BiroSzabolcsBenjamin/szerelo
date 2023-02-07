fbe42c002725a94864a83905ba400ac5fbe694ea
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2023-02-07 09:03:54 +0100

    3f

--- mester.py
+++ mester.py
@@ -1,4 +1,4 @@
-file=open("szerelo.txt","r")
+file=open("szerelo.txt","r",encoding="UTF-8")
 file2=open("abc.txt","w")
 file3=open("ujszerelo.txt","w")
 file_data=[]
@@ -12,3 +12,12 @@ for i in file:
         file_data.append(i.split('\t'))
 
 del file_data[0]
+
+#3.
+for i in range(len(file_data)):
+    if(file_data[i][2]>="2000"):
+        print(file_data[i][1],file_data[i][2],"ben ált munkába")
+
+#4.
+
+#5.
