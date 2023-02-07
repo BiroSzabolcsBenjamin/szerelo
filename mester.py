dc323ee608097e048e46a13daf1d661162952d2d
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2023-02-07 10:33:46 +0100

    f4

--- mester.py
+++ mester.py
@@ -1,6 +1,6 @@
 file=open("szerelo.txt","r",encoding="UTF-8")
 file2=open("abc.txt","w")
-file3=open("ujszerelo.txt","w")
+file3=open("ujszerelo.txt","w",encoding="UTF-8")
 file_data=[]
 file_data2=[]
 file_data3=[]
@@ -19,5 +19,14 @@ for i in range(len(file_data)):
         print(file_data[i][1],file_data[i][2],"ben ált munkába")
 
 #4.
+for i in range(len(file_data)):
+    file_data.sort()
+    file2.write(file_data[i][1]+"\n")
+
+
 
 #5.
+for i in range(len(file_data)):
+    del file_data[i][0]
+    file3.write(file_data[i][0]+"\t"+file_data[i][1]+"\n")
+
