4bdd606ae1c3aa39c35ae609c5497e5e0b2ab6ff
Biró Szabolcs Benjámin <szabi070318@gmail.com>
2023-02-12 19:55:07 +0100

    2f

--- mester.py
+++ mester.py
@@ -17,14 +17,9 @@ del file_data[0]
 print("Ennyi szerelő van: ",len(file_data))
 
 #2.
-for i in file:
-    if(i[-1]=='\n'):
-        file_data.append(i[:-1].split('\t'))
-    else:    
-        file_data.append(i.split('\t'))
-    
-
-
+for i in range(len(file_data)):
+    if(file_data[0][2]>file_data[i][2]):
+        print(file_data[i][1],"a legnagyobb munka tapasztalattal rendelkező szerelő.")
 
 #3.
 for i in range(len(file_data)):
@@ -37,8 +32,9 @@ for i in range(len(file_data)):
     file2.write(file_data[i][1]+"\n")
 
 
-
 #5.
 for i in range(len(file_data)):
     del file_data[i][0]
     file3.write(file_data[i][0]+"\t"+file_data[i][1]+"\n")
+
+
